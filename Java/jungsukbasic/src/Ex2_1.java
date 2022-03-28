f275a56b6cb1bbaca27e6be5af29f748bd6a4930
daeyeob kim <kimdyk2@hanmail.net>
2022-03-28 23:40:42 +0900

    자바 깃허브 커밋 테스트

---
+++ Java/jungsukbasic/src/Ex2_1.java
@@ -0,0 +1,7 @@
+public class Ex2_1 {
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+        System.out.println("Hello");
+        System.out.println("깃허브 커밋 테스트용 클래스");
+    }
+}
