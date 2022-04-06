d7ca60858dbc6eea7c91710657e791e220c82ed0
daeyeob kim <kimdyk2@hanmail.net>
2022-04-06 13:54:39 +0900

    클래스 연습3-성적표

---
+++ Java/TIL/SpringAndCloudCurriculum/1d/src/step3/Grade.java
@@ -0,0 +1,17 @@
+package step3;
+
+public class Grade {   // 데이터를 찍어낼 틀을 생성
+    /*
+    * 클래스에 항목 지정
+    -> 이름 국어 영어 수학 총점 평균
+    * */
+
+    String name;
+
+    int kor;
+    int eng;
+    int math;
+    int total;
+    float avg;
+
+}
